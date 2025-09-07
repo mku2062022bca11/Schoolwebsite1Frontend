286c95901e0662cef479bccefa130f5886897195
mku2062022bca11 <sakthivelk8340@gmail.com>
2025-09-07 15:03:25 +0530

    First commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-6 text-center">
-      <p>© 2023 QB365 Public School. All rights reserved.</p>
+      <p>© 2023 Schools Public School. All rights reserved.</p>
     </footer>
   );
 };
